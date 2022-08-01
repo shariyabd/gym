b9ea8786cc59a8cd14f422432e0d8c6d80901ef1
Mohammad Shariya <92425342+shariyabd@users.noreply.github.com>
2022-08-01 14:57:25 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.side-bar').click(function(){
+        $('nav .nav-items').toggleClass('active')
+        $('.side-bar i').toggleClass('active');
+
+    })
+})
+
